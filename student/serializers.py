8dcdea0cbb5783448a55f9604f9e54eafd58e34c
Usama Nadeem <usamanadeem0786@gmail.com>
2023-09-24 23:14:16 +0500

    changed student model according to requirements

--- student/serializers.py
+++ student/serializers.py
@@ -1,23 +1,29 @@
 from rest_framework import serializers
 from .models import Student  # Import your custom Student model from the same app
+from rest_framework.exceptions import ValidationError
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class StudentSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(write_only=True)
+    password = serializers.CharField(write_only=True)
     class Meta:
         model = Student
-        fields = ['email', 'name', 'roll_number', 'password']
+        fields = ['name', 'roll_number','email','password']
 
     def create(self, validated_data):
+
         # Generate a username by combining name and roll_number
         username = f"{validated_data['name']}_{validated_data['roll_number']}"
-
+        new_user = User.objects.create(username=username,email=validated_data['email'],is_student=True)
+        new_user.set_password(validated_data['password'])
+        new_user.save()
         student = Student(
-            email=validated_data['email'],
             name=validated_data['name'],
             roll_number=validated_data['roll_number'],
-            username=username,  # Set the generated username
-            is_student = True
+            user = new_user
         )
-        student.set_password(validated_data['password'])
+        
         student.save()
         return student
